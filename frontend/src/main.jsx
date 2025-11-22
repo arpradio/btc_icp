3adc4e67771c62514cbd3c7c92071b4352448b6d
psyenceLab <contact@psyencelab.media>
2025-11-22 11:38:41 -0700

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import nexLogo from '/logo.png';
 import '../index.css';
+import Wallet from "./wallet.jsx"
+
 
 function App() {
   return (
-    <div className="flex h-screen items-center justify-center bg-purple-600">
+  
+    <div className="flex flex-col h-screen items-center justify-center  bg-purple-600">
       <img src={nexLogo} alt="NexBTC Logo" className="h-80 w-80" />
       <p className="text-center w-full">Your Portal to BTC Defi</p>
+
+     <Wallet/>
     </div>
+    
+  
   );
 }
 
