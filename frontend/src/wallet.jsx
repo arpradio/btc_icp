1fbbd0877d6ec0506f4dcab75dfc76d3e6a7125f
psyenceLab <contact@psyencelab.media>
2025-11-22 11:39:16 -0700

    Create wallet.jsx

---
+++ frontend/src/wallet.jsx
@@ -0,0 +1,8 @@
+function Wallet() {
+  return (
+    <div className="flex w-fit p-2 mt-2 items-center border-neutral-300 border hover:scale-105 hover:bold">
+   Wallet
+     </div>
+  );
+}
+export default Wallet;
