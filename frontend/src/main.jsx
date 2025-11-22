57fdd8fcb660974c64ae862153855250dee1ca05
arpradio <contact@psyencelab.media>
2025-11-22 10:44:04 -0700

    canister push

---
+++ frontend/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import nexLogo from '/logo.png';
+import '../index.css';
+
+function App() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-purple-600">
+      <img src={nexLogo} alt="NexBTC Logo" className="h-80 w-80" />
+      <p className="text-center w-full">Your Portal to BTC Defi</p>
+    </div>
+  );
+}
+
+export default App;
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
